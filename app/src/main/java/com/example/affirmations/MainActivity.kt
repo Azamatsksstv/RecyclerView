7bfe5e1023a6f5064b178a70ee3fa87c613cdccd
Azamat Zhambylbayev <200103392@stu.sdu.edu.kz>
2022-10-02 16:31:18 +0500

    4. Adding a RecyclerView to your app - Modify the MainActivity to use a RecyclerView(7)

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -14,5 +14,6 @@ class MainActivity : AppCompatActivity() {
         val myDataset = Datasource().loadAffirmations()
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
